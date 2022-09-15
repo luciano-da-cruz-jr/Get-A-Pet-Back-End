322a95a88dcee6546b82e773e69892129590b4a4
Luciano da Cruz Junior <lucianocruzjunior@gmail.com>
2022-09-15 17:55:34 -0300

    token no register user implementado 242 V1

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,5 +1,6 @@
 const User = require('../models/User')
 const bcrypt = require('bcrypt')
+const createUserToken = require('../helpers/create-user-token')
 
 module.exports = class UserController {
     static async register(req, res) {
@@ -60,11 +61,8 @@ module.exports = class UserController {
 
         try {
             const newUser = await user.save()
-            res.status(201).json({
-                message: 'Usuário criado com sucesso!',
-                newUser,
-            })
-            return
+           
+            await createUserToken(newUser, req, res)
         } catch (error) {
             res.status(500).json({ message: error })
         }
