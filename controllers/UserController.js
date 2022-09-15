273489ca2044a51bcae023456039ce2e5ccc407f
Luciano da Cruz Junior <lucianocruzjunior@gmail.com>
2022-09-15 18:18:41 -0300

    login user implementado 243 V1

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -67,4 +67,39 @@ module.exports = class UserController {
             res.status(500).json({ message: error })
         }
     }
+
+    static async login(req, res){
+        const { email, password } = req.body
+
+        if (!email) {
+            res.status(422).json({ message: 'O e-mail é obrigatório' })
+            return
+        }
+        
+        if (!password) {
+            res.status(422).json({ message: 'A senha é obrigatória' })
+            return
+        }
+
+         // check if user existis
+         const user = await User.findOne({ email: email })
+
+         if (!user) {
+             res.status(422).json({
+                 message: 'Não há usuário cadastrado com este e-mail!',
+             })
+             return
+         }
+
+         const checkPassword = await bcrypt.compare(password, user.password)
+
+         if(!checkPassword){
+            res.status(422).json({
+                message: 'Senha inválida!',
+            })
+            return
+         }
+
+         await createUserToken(user, req, res)
+    }
 }
