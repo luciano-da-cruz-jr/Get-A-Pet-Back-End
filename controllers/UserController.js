c238ea5136ddc8a83dad1f45a0aa88f7c389bf86
Luciano da Cruz Junior <lucianocruzjunior@gmail.com>
2022-09-15 18:50:22 -0300

    resgatando user pelo token implementado 245 V1

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,6 +1,10 @@
 const User = require('../models/User')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+// helpers
 const createUserToken = require('../helpers/create-user-token')
+const getToken = require('../helpers/get-token')
 
 module.exports = class UserController {
     static async register(req, res) {
@@ -110,6 +114,12 @@ module.exports = class UserController {
         console.log(req.headers.authorization)
 
         if (req.headers.authorization) {
+            const token = getToken(req)
+            const decoded = jwt.verify(token, 'nossosecret')
+
+            currentUser = await User.findById(decoded.id)
+
+            currentUser.password = undefined
         } else {
             currentUser = null
         }
